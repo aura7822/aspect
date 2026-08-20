394c5d215b722a3736297ddaa69b765f9b56c989
aura <joshuaura7822@gmail.com>
2026-08-20 16:21:11 +0300

    Update logo and hero media URLs to absolute paths

--- src/data/brand.js
+++ src/data/brand.js
@@ -1,8 +1,8 @@
 // Drop a hosted SVG logo URL here to show it in the navbar, hero, and footer.
-export const LOGO_URL = '../public/favicon.svg'
+export const LOGO_URL = 'https://github.com/aura7822/aspect/blob/master/public/favicon.svg'
 
 // Hero background accepts a video (mp4/webm), an animated gif, or a static image.
 // Set HERO_MEDIA_TYPE to 'video' | 'image' to match whatever URL you provide.
-export const HERO_MEDIA_URL = '../public/1.gif'
+export const HERO_MEDIA_URL = 'https://github.com/aura7822/aspect/blob/master/public/1.gif'
 
 export const HERO_MEDIA_TYPE = 'image' // 'video' | 'image' (gifs use 'image')
